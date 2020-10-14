6ba06951918ccf27781ffff138a2ee3454050f41
Michael Townsend <mike.townsend@fluidly.com>
2020-10-14 21:55:40 +0100

    Replace create-react-app branding with equal experts branding

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('Rendering the app', () => {
+  it('shows Equal Experts branding in the heading', () => {
+    const { getByText } = render(<App />)
+    const header = getByText(/Equal Experts Calculator/i)
+    expect(header).toBeInTheDocument()
+  })
 })
